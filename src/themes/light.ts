a2484d6e6282e81fb9a103bb038334f8e6fc450f
lutie0920 <lucianatie09@gmail.com>
2023-11-29 13:18:56 -0300

    finalizar o projeto

---
+++ src/themes/light.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+const temaLight = {
+  corPrincipal: '#282a35',
+  corSecundaria: '#949494',
+  corDeFundo: '#fff',
+  corDeFundoBotao: '#4476bf',
+  corDaBorda: '#c1c1c1'
+}
+
+export default temaLight
