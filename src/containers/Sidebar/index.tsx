c699ae4e9156752e0f8c0820d6e550ed545d3c04
lutie0920 <lucianatie09@gmail.com>
2023-11-29 12:17:07 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/lutie0920.png" />
-    <Titulo fontSize={20}>Luciana Kamon</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Luciana Kamon</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        lutie0920
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
